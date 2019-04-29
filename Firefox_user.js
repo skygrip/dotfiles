1495e81137b77d4e372d1d72e15a1e03ee765ea6
skygrip <20010424+skygrip@users.noreply.github.com>
2019-04-29 13:01:10 +1000

    Enable built in PDF reader

--- Firefox_user.js
+++ Firefox_user.js
@@ -533,7 +533,7 @@ user_pref("privacy.resistFingerprinting", true);
 // https://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2015-2743
 // https://blog.mozilla.org/security/2015/08/06/firefox-exploit-found-in-the-wild/
 // https://www.mozilla.org/en-US/security/advisories/mfsa2015-69/
-user_pref("pdfjs.disabled", true);
+//user_pref("pdfjs.disabled", true);
 
 // PREF: Disable collection/sending of the health report (healthreport.sqlite*)
 // https://support.mozilla.org/en-US/kb/firefox-health-report-understand-your-browser-perf
