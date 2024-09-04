94625ec999edc1734eddbdae836f4ac82df2b50f
Glen <20010424+skygrip@users.noreply.github.com>
2024-09-04 17:18:26 +1000

    Update Win11-Setup.ps1

--- Win11-Setup.ps1
+++ Win11-Setup.ps1
@@ -38,6 +38,92 @@ Function HideGallery {
 	Set-ItemProperty -Path "HKCU:\Software\Classes\CLSID\{e88865ea-0e1c-4e20-9aa6-edcd0212c87c}" -Name "System.IsPinnedToNameSpaceTree" -Type DWord -Value 0	
 }
 
+Function DisableStartWebSearch {
+	#Disables Web Search in Start Menu
+	write-output "Disabling Bing Search in Start Menu"
+	$WebSearch = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"
+	If (!(Test-Path $WebSearch)) {
+		New-Item $WebSearch
+	}
+	Set-ItemProperty $WebSearch DisableWebSearch -Value 1
+	##Loop through all user SIDs in the registry and disable Bing Search
+	foreach ($sid in $UserSIDs) {
+		$WebSearch = "Registry::HKU\$sid\SOFTWARE\Microsoft\Windows\CurrentVersion\Search"
+		If (!(Test-Path $WebSearch)) {
+			New-Item $WebSearch
+		}
+		Set-ItemProperty $WebSearch BingSearchEnabled -Value 0
+	}
+
+	Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" BingSearchEnabled -Value 0
+}
+
+Function HideLearnAboutPicture {
+	#Turn off Learn about this picture
+	write-output "Disabling Learn about this picture"
+	$picture = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel'
+	If (Test-Path $picture) {
+		Set-ItemProperty $picture -Name "{2cc5ca98-6485-489a-920e-b3e88a6ccce3}" -Value 1
+	}
+}
+
+Function DisableRecall {
+	#Turn off Recall
+	write-output "Disabling Recall"
+	$recall = "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsAI"
+	If (!(Test-Path $recall)) {
+		New-Item $recall
+	}
+	Set-ItemProperty $recall DisableAIDataAnalysis -Value 1
+
+	$recalluser = 'HKCU:\SOFTWARE\Policies\Microsoft\Windows\WindowsAI'
+	If (!(Test-Path $recalluser)) {
+		New-Item $recalluser
+	}
+	Set-ItemProperty $recalluser DisableAIDataAnalysis -Value 1
+}
+
+Function DisableWindowsFeedback {
+	#Disables Windows Feedback Experience
+	write-output "Disabling Windows Feedback Experience program"
+	$Advertising = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo"
+	If (!(Test-Path $Advertising)) {
+		New-Item $Advertising
+	}
+	If (Test-Path $Advertising) {
+		Set-ItemProperty $Advertising Enabled -Value 0
+	}
+
+	#Stops the Windows Feedback Experience from sending anonymous data
+	write-output "Stopping the Windows Feedback Experience program"
+	$Period = "HKCU:\Software\Microsoft\Siuf\Rules"
+	If (!(Test-Path $Period)) {
+		New-Item $Period
+	}
+	Set-ItemProperty $Period PeriodInNanoSeconds -Value 0
+}
+
+Function DisableAppSuggestions {
+	#Prevents bloatware applications from returning and removes Start Menu suggestions
+	write-output "Adding Registry key to prevent bloatware apps from returning"
+	$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent"
+	$registryOEM = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager"
+	If (!(Test-Path $registryPath)) {
+		New-Item $registryPath
+	}
+	Set-ItemProperty $registryPath DisableWindowsConsumerFeatures -Value 1
+
+	If (!(Test-Path $registryOEM)) {
+		New-Item $registryOEM
+	}
+	Set-ItemProperty $registryOEM  ContentDeliveryAllowed -Value 0
+	Set-ItemProperty $registryOEM  OemPreInstalledAppsEnabled -Value 0
+	Set-ItemProperty $registryOEM  PreInstalledAppsEnabled -Value 0
+	Set-ItemProperty $registryOEM  PreInstalledAppsEverEnabled -Value 0
+	Set-ItemProperty $registryOEM  SilentInstalledAppsEnabled -Value 0
+	Set-ItemProperty $registryOEM  SystemPaneSuggestionsEnabled -Value 0
+}
+
 ## END FUNCTIONS
 
 EnableClearRecentFiles
@@ -45,3 +131,9 @@ EnableNTFSLongPaths
 SetBIOSTimeUTC
 HideLanguageBar
 HideGallery
+DisableStartWebSearch
+HidePeopleIcon
+HideLearnAboutPicture
+DisableRecall
+DisableWindowsFeedback
+DisableAppSuggestions
